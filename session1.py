9bba77d6e5d80a02e9f0edd7da836666ad28fddf
mukhadis yaru <mukhadis.yaru@gmail.com>
2025-06-04 16:11:01 +0100

    Session 1
    Some basic printing functionality. Printed a simple string and integer incorporating operators

---
+++ session1.py
@@ -0,0 +1,7 @@
+## Some basic python code, introduction to the language.
+
+## Print statement, basic print text to the terminal
+print("Hello world!")
+
+## Print statement, print "*" 10 times
+print("*" * 10)
