0d7febff2334e50901f6b9bc9a86bd8d31580872
mukhadis yaru <mukhadis.yaru@gmail.com>
2025-06-04 18:01:53 +0100

    Adding session 3, this includes some basic python code. 1 - take in and print strings, 2 - print the length of a string

---
+++ session3.py
@@ -0,0 +1,29 @@
+# Some basic python problems to practice
+
+# Code 1 takes in your first and last name as 2 seperate inputs and returns your full name
+print("Running practice code 1")
+print("\n")
+
+print("Hi, I want to get to know you!")
+print("\n")
+
+first_name = input("What is your first name? \n")
+print("\n")
+
+second_name = input("Awesome! What is your last name?\n")
+print("\n")
+
+full_name = first_name + " " + second_name
+
+print(full_name + ", pleasure to meet you!\n My name is Programme! but you can call me P\n")
+print("\n")
+
+# Code 2 takes in any word and prints how many characters there are
+print("Running practice code 2")
+print("\n")
+
+print("Give me a word, any word, I will tell you how long it is\n")
+
+letter_count = input()
+
+print("Haha, that is", len(letter_count), "letter(s) long")
